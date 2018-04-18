95f8c3f2b20a733ce2208054f702bab7fbfa7367
Temitope Joloko <temitope.joloko@gmail.com>
2018-04-18 14:14:29 +0100

    Revert "Feature - Implement user signup, signin and logout (#1)"
    This reverts commit bc1c4a71fc6ddb7e3a7344a5e89137c80c4e852a.

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :phonenumber, presence: true
-  validates :password, presence: true, on: :create, length: {minimum: 5}
-
-  has_secure_password
-end
