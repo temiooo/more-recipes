e5c62af6ebfd1c25b5c248b600be489bda220812
Temitope Joloko <temitope.joloko@gmail.com>
2018-04-18 14:17:22 +0100

    Feature - Implement user signup, signin and logout  (#3)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to recipes_path, notice: "Thank you for signing up!"
+    else
+      flash[:error] = @user.errors.full_messages[0]
+      redirect_to register_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :phonenumber, :password)
+  end
+end
